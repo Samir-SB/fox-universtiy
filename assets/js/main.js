6b1eac91d58a9a5aa7d3b5d94394d673664e7127
solo054 <bs_solo054@yahoo.fr>
2020-10-06 11:48:44 +0100

    styling boxes

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper('.swiper-container', {
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
